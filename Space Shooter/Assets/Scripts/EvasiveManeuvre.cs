3702ed6e95c58ac637b7b3ffdfe6e69ed2fc2430
samth8 <samth8@users.noreply.github.com>
2017-11-17 16:13:54 +0200

    First Commit
    1st Game Version

---
+++ Space Shooter/Assets/Scripts/EvasiveManeuvre.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvasiveManeuvre : MonoBehaviour {
+    public Vector2 startWait;
+    public float dodge;
+    public float smoothing;
+    public Vector2 maneuverTime;
+    public Vector2 maneuverWait;
+    public Boundary boundary;
+    public float tilt;
+
+    private float currentSpeed;
+    private Rigidbody rb;
+    private float targetmaneuver;
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        StartCoroutine (Evade ());
+        currentSpeed = rb.velocity.z;
+	}
+
+    IEnumerator Evade()
+    {
+        yield return new WaitForSeconds(Random.Range(startWait.x, startWait.y));
+        while (true)
+        {
+            targetmaneuver = Random.Range(1, dodge) * -Mathf.Sign(transform.position.x) ;
+            yield return new WaitForSeconds(Random.Range(maneuverTime.x, maneuverTime.y));
+            targetmaneuver = 0;
+            yield return new WaitForSeconds(Random.Range(maneuverWait.x, maneuverWait.y));
+
+        }
+    }
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float newmaneuver = Mathf.MoveTowards(rb.velocity.x, targetmaneuver, Time.deltaTime*smoothing);
+        rb.velocity = new Vector3(newmaneuver, 0.0f, currentSpeed);
+        rb.position = new Vector3
+            (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+
+            );
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+	}
+}
---
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private AudioSource audioscource;
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    private float nextFire;
+    
+
+    void Start()
+    {
+        audioscource = GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            audioscource.Play();
+        }
+
+        
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        rb = GetComponent<Rigidbody>();
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = movement * speed;
+
+        rb.position = new Vector3(
+        Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+        0.0f,
+        Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+         );
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+
+    }
+}
+
